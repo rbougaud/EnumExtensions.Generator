de70de7c9af90f300809e7bd5ecf5aacadbb66a7
Régis BOUGAUD <r.bougaud@ressources-consultants.fr>
2026-02-16 15:51:03 +0100

    add Helper

---
+++ EnumExtensions.Tool/EnumExtensions.Tool/Helper.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace EnumExtensions.Tool;
+
+internal static class Helper
+{
+    internal static IEnumerable<INamedTypeSymbol> GetNamespaceTypes(this INamespaceSymbol ns)
+    {
+        foreach (var member in ns.GetMembers())
+        {
+            if (member is INamespaceSymbol nestedNs)
+            {
+                foreach (var type in nestedNs.GetNamespaceTypes())
+                    yield return type;
+            }
+            else if (member is INamedTypeSymbol type)
+            {
+                yield return type;
+            }
+        }
+    }
+
+    internal static IEnumerable<INamedTypeSymbol> GetEnums(Compilation compilation)
+    {
+        return compilation.Assembly
+            .GlobalNamespace
+            .GetNamespaceTypes()
+            .Where(t => t.TypeKind == TypeKind.Enum)
+            .Where(t => t.GetAttributes()
+                .Any(a => a.AttributeClass?.Name == "GenerateEnumExtensionsAttribute"));
+    }
+
+}
